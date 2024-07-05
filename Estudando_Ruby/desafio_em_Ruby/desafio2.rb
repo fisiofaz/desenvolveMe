248ab470db369e5674040c94bb7388b53d9001d0
fisiofaz <fisiofaz8@gmail.com>
2024-07-05 18:12:11 -0400

    feito: Realizado 3 desafio intermediario em  Ruby

---
+++ Estudando_Ruby/desafio_em_Ruby/desafio2.rb
@@ -0,0 +1,23 @@
+# Desafio
+# Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+# Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++), assumem que o resultado da divisão entre dois inteiros é outro inteiro.
+# Entrada
+# O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio da esfera.
+# Saída
+# A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes e um espaço depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.
+
+#Desafios Ruby na DIO têm funções "gets" e "puts" acessíveis globalmente:
+# "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+# "puts": imprime um texto de saída (output), pulando linha.
+# ".to_i": Converte para int
+
+# Definição de PI
+PI = 3.14159
+# Leitura do valor do raio
+raio = gets().to_f
+#TODO:  Crie as condições necessárias para o programa calcular o volume
+# Cálculo do volume da esfera
+volume = (4.0 / 3.0) * PI * (raio ** 3)
+
+# Exibição do resultado com 3 casas decimais
+puts "VOLUME = %.3f" % volume
