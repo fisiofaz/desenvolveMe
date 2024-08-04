c89ee315b91a1a3919d6857ca963b5cdb31432e8
Fábio André Zatta <75371386+fisiofaz@users.noreply.github.com>
2024-08-04 06:32:09 -0300

    feito: POO - herança

---
+++ Estudando_Ruby/POO_Ruby/herenca.rb
@@ -0,0 +1,19 @@
+# Exemplos Animais
+
+class Animal
+  def dormir
+    puts "zzzzzzzz"
+  end
+
+  def pular
+    "tóin, tóin"
+  end
+end
+
+class gato < Animal
+  def miar
+    'miau'
+  end
+end
+
+gato = Gato.new
